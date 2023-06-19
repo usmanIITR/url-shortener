3e1e1a10d7af6eb03a1f5b328d46cd29781c62a4
Usman Khatri <ugk25880@gmail.com>
2023-06-19 13:45:44 +0530

    fix:jwt authentication and also added some buttons

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const ShortUrl = require("./models/shortUrl");
 const User = require("./models/userModel");
 const bcrypt = require("bcrypt");
+const cookieParser = require('cookie-parser');
 const jwt = require("jsonwebtoken");
 const { v4: uuidv4 } = require('uuid');
 
@@ -16,25 +17,41 @@ app.set("view engine" , "ejs");
 
 app.use(express.urlencoded({extended : false}));
 app.use(express.json());
+app.use(cookieParser());
 
-app.get("/" ,authenticateToken, async (req, res) => {
-    console.log("Authenticated!!!");
+app.get("/" , async (req, res) => {
     try{
-        const user = req.user;
-        if(user == null) {
-            return res.status(404).send("User Not Found");
+        const accessToken = req.cookies.token;
+        if(accessToken){
+            const user = jwt.verify(accessToken , process.env.ACCESS_TOKEN_SECRET);
+            console.log(user);
+            if(user == null) {
+                return res.status(404).send("User Not Found");
+            }
+            const shortUrls = await ShortUrl.find({userId: user.userId});
+            res.render("index" , {shortUrls : shortUrls});
         }
-        const shortUrls = await ShortUrl.find({userId: user.userId});
-        res.render("index" , {shortUrls : shortUrls});
+        else{
+            console.log("hererere");
+            res.clearCookie("token");
+            res.status(500).json({message: "Something went wrong"});
+        }
     }
     catch(error){
-        console.log("error while going to home page");
         console.log(error);
+        res.clearCookie("token");
+        res.status(500).json({message: "Something went wrong"});
     }
 });
 
 app.get("/login", async (req, res) => {
     res.render("login");
+});
+
+app.get("/logout", async (req, res) => {
+    res.clearCookie("token");
+    console.log("Cookie Cleared -> Session Ends");
+    res.redirect("/login");
 });
 
 
@@ -50,9 +67,9 @@ app.post("/login" ,async (req,res) =>{
     try{
         if(await bcrypt.compare(req.body.password , user.password)){
             const userObject = user.toObject();
-            const accessToken = jwt.sign(userObject , process.env.ACCESS_TOKEN_SECRET);
-            res.status(201).json({user : user, accessToken : accessToken});
-            res.redirect('/');
+            const accessToken = jwt.sign(userObject , process.env.ACCESS_TOKEN_SECRET, {expiresIn : "1h"});
+            res.cookie("token", accessToken);
+            res.redirect("/");
         }
         else{
             res.status(400).json({message: "Invalid Credentials"});
@@ -63,38 +80,33 @@ app.post("/login" ,async (req,res) =>{
         return res.status(500).json({message : "something went wrong!"});
     }
 })
-function authenticateToken(req, res, next){
-    console.log("HTTP Request -> " + req.method + " Url -> " + req.url);
-    try{
-        const accessToken = req.query.accessToken;
-        console.log("In AuthMiddleware");
-        console.log(accessToken);
-        if(accessToken){
-            const user = jwt.verify(accessToken , process.env.ACCESS_TOKEN_SECRET);
-            console.log(user);
-            req.user = user;
-            next();
-        }
-        else{
-            console.log("hererere");
-            res.status(500).json({message: "Something went wrong"});
-        }
-    }
-    catch(error){
-        console.log(error);
-        res.status(500).json({message: "Something went wrong"});
-    }
-}
+// function authenticateToken(req, res, next){
+//     console.log("HTTP Request -> " + req.method + " Url -> " + req.url);
+//     try{
+//         const accessToken = req.query.accessToken;
+//         console.log("In AuthMiddleware");
+//         console.log(accessToken);
+//         if(accessToken){
+//             const user = jwt.verify(accessToken , process.env.ACCESS_TOKEN_SECRET);
+//             console.log(user);
+//             req.user = user;
+//             next();
+//         }
+//         else{
+//             console.log("hererere");
+//             res.clearCookie("token");
+//             res.status(500).json({message: "Something went wrong"});
+//         }
+//     }
+//     catch(error){
+//         console.log(error);
+//         res.clearCookie("token");
+//         res.status(500).json({message: "Something went wrong"});
+//     }
+// }
 
 app.post("/signup" , async (req,res) =>{
     try{
-        // console.log(req);
-        // req.body.name = req.body.name.toString();
-        // req.body.email = req.body.email.toString();
-        // req.body.password = req.body.password.toString();
-        // console.log("In SIGNUP");
-        // console.log(req);
-        // console.log(req.body.name , req.body.email , req.body.password);
         const alreadyExist = await User.findOne({email : req.body.email});
         if(alreadyExist) {
             return res.status(400).json({message : "Email Already Exists"});
@@ -108,12 +120,11 @@ app.post("/signup" , async (req,res) =>{
             urls:[],
         });
         const userObject = user.toObject();
-        const accessToken = jwt.sign(userObject , process.env.ACCESS_TOKEN_SECRET);
+        const accessToken = jwt.sign(userObject , process.env.ACCESS_TOKEN_SECRET, {expiresIn : "1h"});
+        res.cookie("token", accessToken);
         console.log("In SignUp -> AccesToken is -> " + accessToken);
         if(accessToken){
-            let url = "/?accessToken=";
-            url = url + accessToken;
-            return res.redirect(url);
+            return res.redirect("/");
         }
         else return res.status(500).json({message : "something went wrong!"});
     }
@@ -126,15 +137,24 @@ app.post("/signup" , async (req,res) =>{
 app.get("/users" ,async (req, res)=>{
     const users = await User.find();
     res.json(users);
-}) // just to debug. xx
+}) // just to debug.
 
 app.post("/shortUrls" , async (req,res) =>{
     try{
-        const userId = (req.session.user).userId;
-        // console.log(userId);
-        const xx = await ShortUrl.create({userId : userId, full : req.body.fullUrl});
-        // console.log(xx);
-        res.redirect('/');
+        const accessToken = req.cookies.token;
+        if(accessToken){
+            const user = jwt.verify(accessToken , process.env.ACCESS_TOKEN_SECRET);
+            // console.log(user);
+            if(user == null) {
+                return res.status(404).send("User Not Found");
+            }
+            await ShortUrl.create({userId : user.userId, full : req.body.fullUrl});
+            res.redirect('/');
+        }
+        else{
+            res.clearCookie("token");
+            res.status(500).json({message: "Something went wrong"});
+        }
     }
     catch{
         res.send("Something went wrong!");
